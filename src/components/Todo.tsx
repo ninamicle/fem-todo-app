9ec9d4b356d2ba973393b724636693e85cbce6b9
Nina <nina.micleusanu93@gmail.com>
2024-05-03 19:08:16 +0200

    update style draggable item

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -14,17 +14,21 @@ const TodoItem:React.FC<TodoItemProps> = ({todo, toggleTodo}) => {
     const { attributes, listeners, setNodeRef, transform,isDragging } = useSortable({
         id: todo.id
     });
+
     const style: CSSProperties = {
         zIndex: isDragging ? 1 : 'auto',
         transform: CSS.Translate.toString(transform),
+        border: isDragging ?  '1px solid #94a3b8': undefined,
+        borderRadius: isDragging ? '5px' : undefined,
+        padding: isDragging ? "1rem": undefined,
     };
     return (
         <div
             ref={setNodeRef} style={style ? style : undefined} {...attributes} {...listeners}
-            className="flex dark:text-lightGrayishBlue text-veryDarkDesaturatedBlueDark gap-4 items-center">
+            className="flex dark:text-lightGrayishBlue text-veryDarkDesaturatedBlueDark gap-4 items-center w-full touch-none">
             <div className="hover:bg-checkBackground dark:bg-veryDarkGrayishBlueDarker bg-lightGrayishBlue rounded-full p-[1px] cursor-pointer">
                 {todo.completed ?
-                    <div className="flex justify-center items-center bg-checkBackground w-6 h-6  rounded-full  touch-none"
+                    <div className="flex justify-center items-center bg-checkBackground w-6 h-6  rounded-full"
                          onClick={() => {
                              return toggleTodo(todo.id)
                          }}>
